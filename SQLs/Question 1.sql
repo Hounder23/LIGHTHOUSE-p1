ba4b5ead192d86558ca62d00b7dad1072c3fd8e4
Theofilos Plessas <theo.plessas@gmail.com>
2024-02-19 16:03:43 -0500

    Create Question 1.sql

---
+++ SQLs/Question 1.sql
@@ -0,0 +1,18 @@
+-- **Question 1: Which cities and countries have the highest level of transaction revenues on the site?**
+
+SELECT CITY,
+	SUM(TOTALTRANSACTIONREVENUE) AS SUM_OF_TRANSACTION_REVENUE
+FROM ALL_SESSIONS
+WHERE CITY != '(not set)'--exclude the sum of all not set city
+GROUP BY CITY
+HAVING SUM(TOTALTRANSACTIONREVENUE) IS NOT NULL --exclude all null sums from output
+ORDER BY SUM(TOTALTRANSACTIONREVENUE) DESC;--order from largest to smallest sum
+
+
+SELECT COUNTRY,
+	SUM(TOTALTRANSACTIONREVENUE) AS SUM_OF_TRANSACTION_REVENUE
+FROM ALL_SESSIONS
+WHERE COUNTRY != '(not set)' -- exclude the sum of not set country
+GROUP BY COUNTRY
+HAVING SUM(TOTALTRANSACTIONREVENUE) IS NOT NULL --exclude all null sums from output
+ORDER BY SUM(TOTALTRANSACTIONREVENUE) DESC;--order from largest to smallest sum
