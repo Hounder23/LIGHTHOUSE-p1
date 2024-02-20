270f9dfee450e04eac78bc93f9620c7db347abbc
Theofilos Plessas <theo.plessas@gmail.com>
2024-02-20 15:39:58 -0500

    Update Question 3.sql

--- SQLs/Question 3.sql
+++ SQLs/Question 3.sql
@@ -1,27 +1,35 @@
 -- **Question 3: Is there any pattern in the types (product categories) of products ordered
 -- from visitors in each city and country?**
 
-WITH MODE_CURRENCYCODE AS --this cte is used to clean up the query a little bit
-	(SELECT CURRENCYCODE  -- other wise there would have been 2 subsqueries to try and read though in th main query
-		FROM
-			(SELECT CURRENCYCODE,
-					COUNT(*) AS FREQUENCY
-				FROM ALL_SESSIONS
-				WHERE COUNTRY != '(not set)'
-					AND V2PRODUCTCATEGORY != 'not set'
-				GROUP BY CURRENCYCODE
-				ORDER BY COUNT(*) DESC
-				LIMIT 1) AS MODE_SUBQUERY)
+
 SELECT 
 	COUNTRY,
 	V2PRODUCTCATEGORY,
-	COUNT(V2PRODUCTCATEGORY) 	AS CATEGORY_COUNT,
-	AVG(PRODUCTPRICE) 			AS AVG_PRODUCTPRICE,
-	AVG(TIMEONSITE) 			AS AVG_TIMEONSITE,
-	(SELECT CURRENCYCODE FROM MODE_CURRENCYCODE) AS MODE_CURRENCY
+	COUNT(V2PRODUCTCATEGORY) 	AS CATEGORY_POPULARITY, 	--show popularity of this category 
+	AVG(PRODUCTPRICE) 			AS AVG_PRODUCTPRICE,-- show avg price in this category
+	CURRENCYCODE,									
+	AVG(TIMEONSITE) 			AS AVG_TIMEONSITE 	-- show avg time spent on site for this category
 FROM ALL_SESSIONS
 WHERE COUNTRY != '(not set)'
-	AND V2PRODUCTCATEGORY != 'not set'
+	AND V2PRODUCTCATEGORY != '(not set)'
 GROUP BY COUNTRY,
-	V2PRODUCTCATEGORY
-ORDER BY COUNTRY, AVG_PRODUCTPRICE desc;
+	V2PRODUCTCATEGORY,
+	CURRENCYCODE
+ORDER BY 	--COUNTRY, 
+			CATEGORY_POPULARITY desc;
+
+SELECT 
+	CITY,
+	V2PRODUCTCATEGORY,
+	COUNT(V2PRODUCTCATEGORY) 	AS CATEGORY_POPULARITY, 	--show popularity of this category 
+	AVG(PRODUCTPRICE) 			AS AVG_PRODUCTPRICE,-- show avg price in this category
+	CURRENCYCODE,									
+	AVG(TIMEONSITE) 			AS AVG_TIMEONSITE 	-- show avg time spent on site for this category
+FROM ALL_SESSIONS
+WHERE CITY != '(not set)'
+	AND V2PRODUCTCATEGORY != '(not set)'
+GROUP BY CITY,
+	V2PRODUCTCATEGORY,
+	CURRENCYCODE
+ORDER BY 	--CITY, 
+			CATEGORY_POPULARITY desc;
