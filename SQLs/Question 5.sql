eb1b1f90526daacefc027238deb2b05a7ef4e106
Theofilos Plessas <theo.plessas@gmail.com>
2024-02-21 11:05:11 -0500

    Update Question 5.sql

--- SQLs/Question 5.sql
+++ SQLs/Question 5.sql
@@ -1,5 +1,15 @@
 -- **Question 5: Can we summarize the impact of revenue generated from each city/country?**
 
-
-
-
+SELECT 
+    City, 
+    Country, 
+    SUM(totaltransactionrevenue/1000000) AS TotalRevenue,
+    SUM(totaltransactionrevenue/1000000) / (SELECT SUM(totaltransactionrevenue/1000000) 
+											FROM public.all_sessions 
+											WHERE city != '(not set)' AND country != '(not set)') 
+											* 100 AS RevenuePercentage
+FROM public.all_sessions
+WHERE city != '(not set)' AND country != '(not set)'
+GROUP BY City, Country
+HAVING SUM(totaltransactionrevenue/1000000) IS NOT NULL
+ORDER BY TotalRevenue DESC;
