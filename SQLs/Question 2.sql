a9eb977bc25679b066f2f0d31c40c95ba85c0802
Theofilos Plessas <theo.plessas@gmail.com>
2024-02-21 10:37:59 -0500

    Update Question 2.sql

--- SQLs/Question 2.sql
+++ SQLs/Question 2.sql
@@ -1,19 +1,25 @@
 -- **Question 2: What is the average number of products ordered from visitors in each city and country?**
 
 SELECT CITY,
-	AVG(PRODUCTQUANTITY) AS AVG_PRODUCT_QUANT_PER_CITY
-FROM ALL_SESSIONS
+	AVG(P.orderedquantity) AS AVG_PRODUCT_QUANT_PER_CITY
+FROM ALL_SESSIONS A
+JOIN PRODUCTS P ON A.PRODUCTSKU = P.SKU
 WHERE CITY != '(not set)' -- do not select the entries named not set as those are invalid for this question
 GROUP BY CITY
-HAVING AVG(PRODUCTQUANTITY) >= 0
-ORDER BY AVG_PRODUCT_QUANT_PER_CITY DESC;
+HAVING AVG(P.orderedquantity) >= 0
+ORDER BY AVG_PRODUCT_QUANT_PER_CITY DESC
+LIMIT 10;
+
+
 
 SELECT COUNTRY,
-	AVG(PRODUCTQUANTITY) AS AVG_PRODUCT_QUANT_PER_COUNTRY
-FROM ALL_SESSIONS
+	AVG(P.orderedquantity) AS AVG_PRODUCT_QUANT_PER_COUNTRY
+FROM ALL_SESSIONS A
+JOIN PRODUCTS P ON A.PRODUCTSKU = P.SKU
 WHERE COUNTRY != '(not set)' -- do not select the entries named not set as those are invalid for this question
 GROUP BY COUNTRY
-HAVING AVG(PRODUCTQUANTITY) >= 0
-ORDER BY AVG_PRODUCT_QUANT_PER_COUNTRY DESC;
+HAVING AVG(P.orderedquantity) >= 0
+ORDER BY AVG_PRODUCT_QUANT_PER_COUNTRY DESC
+LIMIT 10;
 
 
