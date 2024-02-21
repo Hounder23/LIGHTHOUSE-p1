a496c397982960feb346d8630bde3f1aafebf0cd
Theofilos Plessas <theo.plessas@gmail.com>
2024-02-21 11:14:00 -0500

    Update Question 5.sql

--- SQLs/Question 5.sql
+++ SQLs/Question 5.sql
@@ -13,3 +13,16 @@ WHERE city != '(not set)' AND country != '(not set)'
 GROUP BY City, Country
 HAVING SUM(totaltransactionrevenue/1000000) IS NOT NULL
 ORDER BY TotalRevenue DESC;
+
+SELECT  
+    Country, 
+    SUM(totaltransactionrevenue/1000000) AS TotalRevenue,
+    SUM(totaltransactionrevenue/1000000) / (SELECT SUM(totaltransactionrevenue/1000000) 
+											FROM public.all_sessions 
+											WHERE country != '(not set)') 
+											* 100 AS RevenuePercentage
+FROM public.all_sessions
+WHERE country != '(not set)'
+GROUP BY Country
+HAVING SUM(totaltransactionrevenue/1000000) IS NOT NULL
+ORDER BY TotalRevenue DESC;
